1bfd9cde3fffa1b790d4adc76f4c920453de0496
night-light-ghost <nightlight@sent.com>
2024-03-02 18:39:25 -0500

    beginning by organizing all words by connectivity

--- scrap.py
+++ scrap.py
@@ -1,1 +1,34 @@
 # this to serve as where code is to be gathered
+
+
+class Counter(dict):
+    def __missing__(self, key):
+        return 0
+
+frequencies = Counter()
+
+for word in words:
+    for c in word:
+        if frequencies[c]==0:
+            frequencies[c] += 1
+        else:
+            frequencies[c] = frequencies[c] + 1
+
+print("frequencies: ")
+print(frequencies)
+
+weightedWords = {}
+
+for word in words:
+    weight = 0
+    use = 0
+    for c in word:
+        weight += frequencies[c]
+    use = weight / len(word)
+    weightedWords[use] = word
+
+usefulWords = sorted(weightedWords.items(), reverse=True)
+print("weightedWords: ")
+print(usefulWords)
+
+
